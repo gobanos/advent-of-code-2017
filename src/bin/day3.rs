a987cb706ed99b2c6b297070298d1d90c7edc929
Grégory OBANOS <gregory.obanos@gmail.com>
2017-12-03 20:03:55 +0100

    Day 3, part 2 - Not very nice :/

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -4,4 +4,5 @@ use aoc2017::day3;
 
 fn main() {
     println!("Part 1 : {}", day3::part1(361_527));
+    println!("Part 2 : {}", day3::part2(361_527));
 }
--- src/day3.rs
+++ src/day3.rs
@@ -85,6 +85,140 @@ fn diff(a: u32, b: u32) -> u32 {
     if a > b { a - b } else { b - a }
 }
 
+struct SumRing {
+    previous: Option<Box<SumRing>>,
+    ring: Ring,
+    values: Vec<u32>,
+}
+
+impl SumRing {
+    fn new(mut previous: SumRing, ring: Ring) -> SumRing {
+        previous.previous = None;
+
+        let capacity = 8 * ring.index as usize;
+
+        SumRing {
+            previous: Some(Box::new(previous)),
+            ring,
+            values: vec![0; capacity],
+        }
+    }
+
+    fn get(&self, group: usize, index: usize) -> Option<u32> {
+        assert!(group < 4);
+
+        let ring_index = self.ring.index as usize;
+
+        if index < 2 * ring_index {
+            let group_index = 2 * ring_index * group;
+
+            Some(self.values[group_index + index])
+        } else {
+            None
+        }
+    }
+
+    fn get_last(&self, group: usize) -> u32 {
+        assert!(group < 4);
+
+        let next_group_index = 2 * self.ring.index as usize * (group + 1);
+
+        self.values[next_group_index - 1]
+    }
+
+    fn compute(&mut self, x: usize) -> u32 {
+        let mut sum = 0;
+
+        let n = self.ring.index as usize;
+        let group_len = 2 * n;
+
+        let group = x / group_len;
+        let group_index = x % group_len;
+
+        // add previous value
+        if let Some(prev) = x.checked_sub(1) {
+            sum += self.values[prev];
+        }
+
+        // if it's the first value of a group,
+        // add a value before
+        if group_index == 0 {
+            if let Some(prev) = x.checked_sub(2) {
+                sum += self.values[prev];
+            }
+        }
+
+        // if it's one of the two last values of a ring,
+        // add the first value from the ring
+        if 8 * n - x <= 2 {
+            sum += self.values[0];
+        }
+
+        if let Some(ref previous) = self.previous {
+            for offset in (0..3).rev() {
+                if let Some(i) = group_index.checked_sub(offset) {
+                    if let Some(val) = previous.get(group, i) {
+                        sum += val;
+                    }
+                }
+            }
+
+            if group_index < 2 {
+                let previous_group = group.checked_sub(1).unwrap_or(3);
+
+                sum += previous.get_last(previous_group);
+            }
+        }
+
+        self.values[x] = sum;
+
+        sum
+    }
+
+    fn capacity(&self) -> usize {
+        8 * self.ring.index as usize
+    }
+}
+
+fn first_sum() -> SumRing {
+    SumRing {
+        previous: None,
+        ring: Ring {
+            index: 1,
+            first: 2,
+            last: 9,
+        },
+        values: vec![1, 2, 4, 5, 10, 11, 23, 25],
+    }
+}
+
+pub fn part2(input: u32) -> u32 {
+    assert!(input > 25);
+
+    let generator = SpiralGenerator::new();
+
+    let mut previous_sum = Some(first_sum());
+
+    for ring in generator.skip(2) {
+        let sum = {
+            let mut new_sum = SumRing::new(previous_sum.take().unwrap(), ring);
+
+            for x in 0..new_sum.capacity() {
+                let value = new_sum.compute(x);
+                if value > input {
+                    return value;
+                }
+            }
+
+            new_sum
+        };
+
+        previous_sum = Some(sum);
+    }
+
+    panic!("No value found")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -162,5 +296,27 @@ mod tests {
     /// Data from square 1024 must be carried 31 steps.
     fn part1_sample4() {
         assert_eq!(part1(1024), 31);
+    }
+
+    #[test]
+    fn second_sum() {
+        let first_sum = first_sum();
+
+        let mut second_sum = SumRing::new(first_sum, THIRD_RING);
+
+        assert_eq!(second_sum.compute(0), 26);
+        assert_eq!(second_sum.compute(1), 54);
+        assert_eq!(second_sum.compute(2), 57);
+        assert_eq!(second_sum.compute(3), 59);
+        assert_eq!(second_sum.compute(4), 122);
+        assert_eq!(second_sum.compute(5), 133);
+        assert_eq!(second_sum.compute(6), 142);
+        assert_eq!(second_sum.compute(7), 147);
+        assert_eq!(second_sum.compute(8), 304);
+        assert_eq!(second_sum.compute(9), 330);
+        assert_eq!(second_sum.compute(10), 351);
+        assert_eq!(second_sum.compute(11), 362);
+        assert_eq!(second_sum.compute(12), 747);
+        assert_eq!(second_sum.compute(13), 806);
     }
 }
