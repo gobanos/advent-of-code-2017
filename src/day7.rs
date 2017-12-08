ddf2dc47cde1f3c6c127c4dd78e790d9dbe25fc5
Grégory OBANOS <gobanos@virtualbuilding.fr>
2017-12-08 18:01:03 +0100

    Day 7, build a graph.

--- src/day7.rs
+++ src/day7.rs
@@ -1,11 +1,15 @@
 use std::collections::{HashMap, HashSet};
 use day7_parser;
 
+use petgraph::Graph;
+use petgraph::graph::NodeIndex;
+
 #[derive(Debug, Eq, PartialEq)]
 struct Node<'a> {
     name: &'a str,
     weight: u32,
-    children: Vec<&'a str>,
+    total_weight: u32,
+    children: Option<Vec<&'a str>>,
 }
 
 impl<'a> Node<'a> {
@@ -13,7 +17,8 @@ impl<'a> Node<'a> {
         Node {
             name,
             weight,
-            children,
+            total_weight: weight,
+            children: Some(children),
         }
     }
 }
@@ -29,17 +34,42 @@ pub fn part2(input: &str) -> u32 {
 
 
     let root = find_root(&input);
+    let mut root_node = input.remove(root).expect("root not found");
 
-    println!("{:#?}", input.remove(&root[..]).unwrap());
+    let mut graph = Graph::with_capacity(input.len() + 1, input.len());
+    let mut nodes = HashMap::new();
+
+    let children = root_node.children.take().expect("no children found");
+
+    nodes.insert(root, graph.add_node(root_node));
+
+    add_children(&mut graph, &mut input, &mut nodes, root, children);
+
+    println!("{:#?}", graph);
+    println!("{:#?}", input);
 
     unimplemented!()
+}
+
+fn add_children<'a, 'b>(graph: &'b mut Graph<Node<'a>, ()>, input: &'b mut HashMap<&'a str, Node<'a>>, nodes: &'b mut HashMap<&'a str, NodeIndex>, root: &'a str, children: Vec<&'a str>) {
+    for child in children {
+        let mut child_node = input.remove(child).expect("root not found");
+
+        let grand_children = child_node.children.take().expect("no children found");
+
+        nodes.insert(child, graph.add_node(child_node));
+
+        graph.add_edge(nodes[root], nodes[child], ());
+
+        add_children(graph, input, nodes, child, grand_children);
+    }
 }
 
 fn find_root<'a, 'b>(nodes: &'a HashMap<&'b str, Node<'b>>) -> &'b str {
     let mut names = nodes.keys().collect::<HashSet<_>>();
 
     for (_, node) in nodes.iter() {
-        for child in &node.children {
+        for child in node.children.as_ref().unwrap() {
             names.remove(child);
         }
     }
