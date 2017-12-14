439998be14569f8dd421f55566e1f33cf1351797
Grégory OBANOS <gobanos@virtualbuilding.fr>
2017-12-14 09:51:16 +0100

    Add some timing !

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,12 +1,24 @@
 #[macro_use]
 extern crate aoc2017;
+extern crate time;
 
-use aoc2017::day1;
+use aoc2017::day1::*;
 
 fn main() {
     let input = load_input!("day1");
     let input = input.trim();
 
-    println!("Part 1 : {}", day1::part1(input));
-    println!("Part 2 : {}", day1::part2(input));
+    let start_part1 = time::precise_time_s();
+
+    println!("Part 1 : {}", part1(input));
+
+    let start_part2 = time::precise_time_s();
+
+    println!("Part 2 : {}", part2(input));
+
+    let stop = time::precise_time_s();
+
+    println!("\nREPORT:");
+    println!("\t- Part 1: {:.9}s", start_part2 - start_part1);
+    println!("\t- Part 2: {:.9}s", stop - start_part2);
 }
--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,8 +1,20 @@
 extern crate aoc2017;
+extern crate time;
 
-use aoc2017::day3;
+use aoc2017::day3::*;
 
 fn main() {
-    println!("Part 1 : {}", day3::part1(361_527));
-    println!("Part 2 : {}", day3::part2(361_527));
+    let start_part1 = time::precise_time_s();
+
+    println!("Part 1 : {}", part1(361_527));
+
+    let start_part2 = time::precise_time_s();
+
+    println!("Part 2 : {}", part2(361_527));
+
+    let stop = time::precise_time_s();
+
+    println!("\nREPORT:");
+    println!("\t- Part 1: {:.9}s", start_part2 - start_part1);
+    println!("\t- Part 2: {:.9}s", stop - start_part2);
 }
